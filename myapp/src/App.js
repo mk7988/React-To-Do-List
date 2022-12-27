8cac439ee0bb684da4b9294a62d4abff3667a7d9
Manoj <manojkumar120101@gmail.com>
2022-12-27 15:13:24 +0530

    first commit

---
+++ myapp/src/App.js
@@ -0,0 +1,72 @@
+import React,{useState} from 'react'
+import './App.css'
+
+const App = () => {
+  const [todo, setTodo] = useState("")
+  const [todos, setTodos] = useState([])
+  const [editId,setEditId]= useState(0)
+
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+
+    if(editId){
+      const editTodo = todos.find((i)=> i.id===editId);
+      const updatedTodos = todos.map((t)=>
+      t.id === editTodo.id
+      ?(t={id:t.id,todo})
+      :{id:t.id,todo:t.todo}
+      );
+      setTodos(updatedTodos)
+      setEditId(0);
+      setTodo("")
+      return;
+    }
+
+    if(todo !==''){
+      setTodos([{id:`${todo}-${Date.now()}`,todo}, ...todos])
+      setTodo("")
+    }
+  };
+    const handleDelete = (id) =>{
+      const delToDo = todos.filter((to)=> to.id !==id);
+      setTodos([...delToDo])
+
+    }
+    const handleEdit = (id) =>{
+      const editToDo = todos.find((i)=> i.id ===id);
+      setTodo(editToDo.todo)
+      setEditId(id)
+
+    }
+
+  return (
+    
+    <div className='App'>
+      <div className="container">
+        <h1>To Do List</h1>
+        <form className='todoForm' onSubmit={handleSubmit}>
+          <input value={todo} type="text" onChange={(e)=>setTodo(e.target.value)} />
+          <button type='submit'>{editId ? "Edit":"Add"}</button>
+        </form>
+        <ul className='allTodos'>
+          {
+            todos.map((t)=>(
+              <li className='singleTodo'>
+              <span className='todoText' key={t.id}>{t.todo}</span>
+              <button onClick={()=>handleEdit(t.id)}>Edit</button>
+              <button onClick={()=>handleDelete(t.id)}>Delete</button>
+            </li>
+            
+
+            ))
+          }
+         
+        </ul>
+      </div>
+    </div>
+    
+  );
+        };
+
+export default App
